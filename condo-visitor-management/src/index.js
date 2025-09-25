30475504a897aa2ea3571938c19644d3b4c1b3af
Jonathan Melendez <soyjonnymelendez@gmail.com>
2025-09-25 18:23:43 -0400

    change port

--- condo-visitor-management/src/index.js
+++ condo-visitor-management/src/index.js
@@ -4,7 +4,7 @@ const visitorRoutes = require('./routes/visitorRoutes');
 const swagger = require('./config/swagger');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors);
